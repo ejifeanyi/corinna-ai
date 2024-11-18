5dcdb178cf4dc88fa35274393dccd4e02365a9b5
the2minengineer <ifeanyiemmanueljoseph@gmail.com>
2024-11-18 05:09:29 +0100

    custom signup form

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import SignUpFormProvider from "@/app/components/forms/sign-up/form-provider";
+import React from "react";
+import RegistrationFormStep from "@/app/components/forms/sign-up/registration-step";
 
-type Props = {}
+type Props = {};
 
-const page = (props: Props) => {
-  return (
-    <div>page</div>
-  )
-}
+const SignUp = (props: Props) => {
+	return (
+		<div className="flex-1 py-36 md:px-16 w-full">
+			<div className="flex flex-col h-full gap-3">
+				<SignUpFormProvider>
+					<div className="flex flex-col gap-3">
+						<div className="flex flex-col gap-3">
+							<RegistrationFormStep />
+							{/* <ButtonHandler /> */}
+						</div>{" "}
+					</div>
+				</SignUpFormProvider>
+			</div>
+		</div>
+	);
+};
 
-export default page
+export default SignUp;
