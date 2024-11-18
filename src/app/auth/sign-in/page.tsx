e2faf2a6adf411db79f6225a55ec366b56181224
the2minengineer <ifeanyiemmanueljoseph@gmail.com>
2024-11-18 20:00:23 +0100

    sign-in

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import SignInFormProvider from "@/app/components/forms/sign-in/form-provider";
+import LoginForm from "@/app/components/forms/sign-in/login-form";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
-const page = (props: Props) => {
-  return (
-    <div>page</div>
-  )
-}
+const SignIn = (props: Props) => {
+	return (
+		<div className="flex-1 py-36 md:px-16 w-full">
+			<div className="flex flex-col h-full gap-3">
+				<SignInFormProvider>
+					<div className="flex flex-col gap-3">
+						<LoginForm />
+						<div className="w-full flex flex-col gap-3 items-center">
+							<Button
+								type="submit"
+								className="w-full"
+							>
+								Submit
+							</Button>
+							<p>
+								Dont have an account?{" "}
+								<Link
+									href="/auth/sign-up"
+									className="font-bold"
+								>
+									Create One
+								</Link>
+							</p>
+						</div>
+					</div>
+				</SignInFormProvider>
+			</div>
+		</div>
+	);
+};
 
-export default page
+export default SignIn;
